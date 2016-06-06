7550dd37272dd7a39e51d91f682882533aad956e
George Marselis <george@marsel.is>
2016-06-06 04:08:11 +0300

    small bugfix

--- ergastirio_bashs.py
+++ ergastirio_bashs.py
@@ -35,9 +35,9 @@ cursor.execute( createdb_query )
 conn.commit( )
 # init tables
 droptable_query                = 'drop table if exists gene2disease, gene, disease'
-createtable_gene_query         = 'create table if not exists gene( geneId varchar(20) not null, geneName varchar(50) not null, primary key (geneId) )'
-createtable_disease_query      = 'create table if not exists disease( diseaseId varchar(20) not null, diseaseName varchar(50) not null, primary key (diseaseId) )'
-createtable_gene2disease_query = 'create table if not exists gene2disease ( geneId varchar(20) not null, diseaseId varchar(20) not null, foreign key (geneId) references gene(geneId) on update cascade on delete restrict, foreign key (diseaseId) references disease(diseaseId) on update cascade on delete restrict )'
+createtable_gene_query         = 'create table if not exists gene( geneId varchar(50) not null, geneName varchar(100) not null, primary key (geneId) )'
+createtable_disease_query      = 'create table if not exists disease( diseaseId varchar(50) not null, diseaseName varchar(100) not null, description varchar(100) not null, primary key (diseaseId) )'
+createtable_gene2disease_query = 'create table if not exists gene2disease ( geneId varchar(50) not null, diseaseId varchar(50) not null, foreign key (geneId) references gene(geneId) on update cascade on delete restrict, foreign key (diseaseId) references disease(diseaseId) on update cascade on delete restrict )'
 cursor.execute( "use " + db )
 print( droptable_query )
 cursor.execute( droptable_query )
@@ -66,7 +66,7 @@ with open( tsv_file ) as csvfile:
 
 		if row['diseaseId'] not in disease_keys: 
 			disease_keys.append( row['diseaseId'] )
-			insertdiseasedata_query = "insert into disease( diseaseId, diseaseName ) value ( '" + row['diseaseId'] + "', '" + row['geneName'] + "' )"
+			insertdiseasedata_query = "insert into disease( diseaseId, diseaseName, description ) value ( '" + row['diseaseId'] + "', " + conn.escape(row['diseaseName']) + ", " + conn.escape(row['description']) + " )"
 			print( insertdiseasedata_query )
 			cursor.execute( insertdiseasedata_query )
 		cursor.execute( "insert into gene2disease( geneId, diseaseId ) value ( '" + row['geneId'] + "', '" + row['diseaseId'] + "' )" )
