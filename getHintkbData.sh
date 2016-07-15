6738a02c99283d94eb8712cb3a2b35a8fdf2e918
George Marselis <george@marsel.is>
2016-07-15 22:01:00 +0300

    hintkb is useless after getting everyrhing via REST

--- getHintkbData.sh
+++ getHintkbData.sh
@@ -1,3 +1,3 @@
 #!/opt/local/bin/bash
 
-for protein in $( < listOfProteins.csv ) ; do wget --output-document=- http://hintkb.ceid.upatras.gr/api/ppi/byprotein/${protein} | gunzip -c > hintkb/${protein}; done
+for protein in $( < listOfProteins.tsv ) ; do wget --no-verbose --output-document=- http://hintkb.ceid.upatras.gr/api/ppi/byprotein/${protein} | gunzip -c > hintkb/${protein}; done
