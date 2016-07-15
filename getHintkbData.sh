e513f0626dfec7351aacf11dd68bde56806d3239
George Marselis <george@marsel.is>
2016-07-15 22:03:40 +0300

    hintkb shellscript is now useless

--- getHintkbData.sh
+++
@@ -1,3 +0,0 @@
-#!/opt/local/bin/bash
-
-for protein in $( < listOfProteins.tsv ) ; do wget --no-verbose --output-document=- http://hintkb.ceid.upatras.gr/api/ppi/byprotein/${protein} | gunzip -c > hintkb/${protein}; done
