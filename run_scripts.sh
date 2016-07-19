49f993a2f3c84119efc4b9aab29913c1a9ec18f6
George Marselis <george@marsel.is>
2016-07-19 15:14:49 +0300

    run_scripts.sh

---
+++ run_scripts.sh
@@ -0,0 +1,10 @@
+./gEtgenEspublications.py 
+./gEtproteiNsisoforMscount.py
+./gEtproteInexperimentAlverified.py
+./gEtgeNeByscore.py -s .4
+./gEtproteInfunctions.py
+./EtproteiNsByfunction.py -F E
+./gEtproteInisofoRmcouNtBypuBscount.py -p 2
+./gEtgeNeByisofoRmsequence.py -S E
+./gEtproteiNsfunctiOnBynamespace.py -N E
+./gEtgenEsisoforMscouNtBysequence.py -s E
