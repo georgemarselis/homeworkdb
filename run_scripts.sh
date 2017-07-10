22369c13bf8045bd4e029b5aaffeb287bcd907ca
George Marselis <george.marselis@ncmm.uio.no>
2017-07-10 22:06:44 +0200

    fixes

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,10 +1,12 @@
-./gEtgenEspublications.py 
-./gEtproteiNsisoforMscount.py
-./gEtproteInexperimentAlverified.py
-./gEtgeNeByscore.py -s .4
-./gEtproteInfunctions.py
-./EtproteiNsByfunction.py -F E
-./gEtproteInisofoRmcouNtBypuBscount.py -p 2
-./gEtgeNeByisofoRmsequence.py -S E
-./gEtproteiNsfunctiOnBynamespace.py -N E
-./gEtgenEsisoforMscouNtBysequence.py -s E
+#!/bin/bash
+
+./getGenesPublications.py 
+./getProteinsIsoformsCount.py
+./getProteinExperimentalVerified.py
+./getGeneByScore.py -s .4
+./getProteinFunctions.py
+./getProteinsByFunction.py -F E
+./getProteinIsoformCountByPubsCount.py -p 2
+./getGeneByIsoformSequence.py -S E
+./getProteinsFunctionByNamespace.py -N E
+./getGenesIsoformsCountBySequence.py -s E
